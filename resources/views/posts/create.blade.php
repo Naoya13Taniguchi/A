e2eb18f6649e4e607c9bc30c69e971786c4996ac
Naoya13Taniguchi <ec2-user@ip-172-31-74-248.ec2.internal>
2023-08-12 14:54:42 +0900

    エラーメッセージの修正

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -18,7 +18,7 @@
             <div class="body">
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="You did good jobs!!">{{ old('post.body') }}</textarea>
-                <p class="body__error" style="color:red"> {{ $errors->first('post.title')}} </p>
+                <p class="body__error" style="color:red"> {{ $errors->first('post.body')}} </p>
             </div>
             <input type="submit" value="store">
         </form>
