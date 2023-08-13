a3096098c2e69d268e80b54b6b5911d9b3944879
Naoya13Taniguchi <ec2-user@ip-172-31-74-248.ec2.internal>
2023-08-13 21:28:10 +0900

    以前の投稿を編集できるようにしました。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
    public function index(Post $post)
    {
-    return view('posts.index')->with(['posts' => $post->getPaginateBylimit(1)]);
+    return view('posts.index')->with(['posts' => $post->getPaginateBylimit(5)]);
     // blade内で使う変数'posts'と設定。
    }
    
@@ -31,6 +31,19 @@ class PostController extends Controller
        $input = $request['post'];
        $post->fill($input)->save();
        return redirect('/posts/'. $post->id);
+   }
+   
+   public function edit(Post $post)
+   {
+       return view('posts/edit')->with(['post'=> $post]);
+   }
+   
+   public function update(PostRequest $request,Post $post)
+   {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        
+        return redirect('/posts/'. $post->id);
    }
 }
 ?>
